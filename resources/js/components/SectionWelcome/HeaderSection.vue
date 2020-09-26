5bbf293594c6d4debdc1a0a7302bf29663fd9492
alexmurithi <gitsaleks@gmail.com>
2020-09-26 09:46:56 -0400

    responsive navbar bar added

--- resources/js/components/SectionWelcome/HeaderSection.vue
+++ resources/js/components/SectionWelcome/HeaderSection.vue
@@ -1,6 +1,6 @@
 <template>
       <nav class="navbar navbar-dark navbar-expand-md fixed-top bg-dark navbar--apple">
-        <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#menu"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"><i class="la la-navicon"></i></span></button>
+        <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#menu"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span></button>
             <div class="collapse navbar-collapse" id="menu">
                 <ul class="nav navbar-nav flex-grow-1 justify-content-between">
                     <li class="nav-item d-none d-xs-block d-md-block" role="presentation"><a class="nav-link" href="#"><i class="far fa-edit apple-logo"></i></a></li>
